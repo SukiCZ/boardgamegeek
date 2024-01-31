912cf175ce1e77d52fb3a62b14706976b81999a8
Jakub Boukal <www.bagr@gmail.com>
2024-01-31 13:25:28 +0100

    Bump version: 1.1.4 → 1.1.5

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="bgg-api",
-    version="1.1.4",
+    version="1.1.5",
     packages=find_packages(),
     license="BSD",
     author="Jakub Boukal",
