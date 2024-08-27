9e29bdd000343cd4e36a9a4f02f93e31cb37c224
Jakub Boukal <www.bagr@gmail.com>
2024-08-28 00:16:25 +0200

    Bump version: 1.1.8 → 1.1.9

--- boardgamegeek/__init__.py
+++ boardgamegeek/__init__.py
@@ -45,4 +45,4 @@ __all__ = (
     "CacheBackendMemory",
 )
 
-__version__ = "1.1.8"
+__version__ = "1.1.9"
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="bgg-api",
-    version="1.1.8",
+    version="1.1.9",
     packages=find_packages(),
     license="BSD",
     author="Jakub Boukal",
