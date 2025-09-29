12eb53be08c9233246ea0ce29daa13b8b108e91b
Jakub Boukal <www.bagr@gmail.com>
2025-09-29 20:08:07 +0200

    Align cache with mypy

--- boardgamegeek/cache.py
+++ boardgamegeek/cache.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 import requests
 import requests_cache
 
 from .exceptions import BGGValueError
+
+StrOrPath = str | Path
 
 
 class CacheBackend:
@@ -9,29 +13,29 @@ class CacheBackend:
 
 
 class CacheBackendNone(CacheBackend):
-    def __init__(self):
+    def __init__(self) -> None:
         self.cache = requests.Session()
 
 
 class CacheBackendMemory(CacheBackend):
     """Cache HTTP requests in memory"""
 
-    def __init__(self, ttl):
+    def __init__(self, ttl: int):
         try:
             int(ttl)
-        except ValueError:
-            raise BGGValueError
+        except ValueError as e:
+            raise BGGValueError from e
         self.cache = requests_cache.CachedSession(backend="memory", expire_after=ttl, allowable_codes=(200,))
 
 
 class CacheBackendSqlite(CacheBackend):
     """Cache HTTP requests in a SQLite database"""
 
-    def __init__(self, path, ttl, fast_save=True):
+    def __init__(self, path: StrOrPath, ttl: int, fast_save: bool = True):
         try:
             int(ttl)
-        except ValueError:
-            raise BGGValueError
+        except ValueError as e:
+            raise BGGValueError from e
 
         self.cache = requests_cache.CachedSession(
             cache_name=path,
